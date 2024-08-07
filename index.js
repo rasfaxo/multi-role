95237fb10baa296b058ba74fc91cedef56ffe842
Naufal Farraz Al <naufalfarraz30098@gmail.com>
2024-08-08 04:00:12 +0700

    configured session storage using sequelize and added sessions database to store login sessions and added some middleware

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ import express from "express";
 import cors from "cors";
 import session from "express-session";
 import dotenv from "dotenv";
-// import db from "./config/Database.js";
+import db from "./config/Database.js";
+import SequelizeStore from "connect-session-sequelize";
 import UserRoute from "./routes/UserRoute.js"
 import ProductRoute from "./routes/ProductRoute.js"
+import AuthRoute from './routes/AuthRoute.js';
 
 dotenv.config();
 
 const app = express();
+
+const sessionStore = SequelizeStore(session.Store);
+const store = new sessionStore({
+    db: db
+});
 
 // (async()=>{
 //     await db.sync();
@@ -19,6 +26,7 @@ app.use(session({
     secret: process.env.SESS_SECRET,
     resave: true,
     saveUninitialized: true,
+    store: store,
     cookie: {
         secure: 'auto'
     }
@@ -30,6 +38,7 @@ app.use(cors({
 app.use(express.json());
 app.use(UserRoute)
 app.use(ProductRoute)
+app.use(AuthRoute)
 
 app.listen(port, ()=>{
     console.log(`Server up and running on ${port}`)
