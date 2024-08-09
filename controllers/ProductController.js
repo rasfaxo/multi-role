41c8c9e642a8a4227ab9235a6963dc4afd447c0f
Naufal Farraz Al <naufalfarraz30098@gmail.com>
2024-08-09 11:02:11 +0700

    Added new updatedProducts and deleteProduct

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -4,94 +4,159 @@ import User from "../models/userModel.js";
 import { Op } from "sequelize";
 
 export const getProducts = async (req = request, res = response) => {
-    try {
-        let response;
-        if(req.role === "admin"){
-            response = await Product.findAll({
-                attributes:['uuid', 'name', 'price'],
-                include: [{
-                    model: User,
-                    attributes: ['name', 'email']
-                }]
-            })
-            
-        }else{
-            response = await Product.findAll({
-                attributes:['uuid', 'name', 'price'],
-                where:{
-                    userId: req.userId
-                },
-                include: [{
-                    model: User,
-                    attributes: ['name', 'email']
-                }]
-            })
-        }
-        res.status(200).json(response)
-    } catch (error) {
-        res.status(500).json({msg: error.message});
+  try {
+    let response;
+    if (req.role === "admin") {
+      response = await Product.findAll({
+        attributes: ["uuid", "name", "price"],
+        include: [
+          {
+            model: User,
+            attributes: ["name", "email"],
+          },
+        ],
+      });
+    } else {
+      response = await Product.findAll({
+        attributes: ["uuid", "name", "price"],
+        where: {
+          userId: req.userId,
+        },
+        include: [
+          {
+            model: User,
+            attributes: ["name", "email"],
+          },
+        ],
+      });
     }
-}
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
 
 export const getProductsById = async (req = request, res = response) => {
-    try {
-        const product = await Product.findOne({
-            where:{
-                uuid: req.params.id
-            }
-        })
-        if(!product) return res.status(404).json({msg: "product not found"});
-        
-        let response;
-        if(req.role === "admin"){
-            response = await Product.findOne({
-                attributes:['uuid', 'name', 'price'],
-                where:{
-                    id: product.id
-                },
-                include: [{
-                    model: User,
-                    attributes: ['name', 'email']
-                }]
-            })
-            
-        }else{
-            response = await Product.findOne({
-                attributes:['uuid', 'name', 'price'],
-                where:{
-                    [Op.and]:[{id: product.id}, {userId: req.userId}]
-                },
-                include: [{
-                    model: User,
-                    attributes: ['name', 'email']
-                }]
-            })
-        }
-        res.status(200).json(response)
-    } catch (error) {
-        res.status(500).json({msg: error.message});
+  try {
+    const product = await Product.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    if (!product) return res.status(404).json({ msg: "Data not found" });
+
+    let response;
+    if (req.role === "admin") {
+      response = await Product.findOne({
+        attributes: ["uuid", "name", "price"],
+        where: {
+          id: product.id,
+        },
+        include: [
+          {
+            model: User,
+            attributes: ["name", "email"],
+          },
+        ],
+      });
+    } else {
+      response = await Product.findOne({
+        attributes: ["uuid", "name", "price"],
+        where: {
+          [Op.and]: [{ id: product.id }, { userId: req.userId }],
+        },
+        include: [
+          {
+            model: User,
+            attributes: ["name", "email"],
+          },
+        ],
+      });
     }
-}
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
 
 export const createProducts = async (req = request, res = response) => {
-    const {name, price} = req.body;
-    try {
-        await Product.create({
-            name: name,
-            price: price,
-            userId: req.userId,
-            role:  req.role
-        })
-        res.status(201).json({msg: "product created successfully"})
-    } catch (error) {
-        res.status(500).json({msg: error.message});
+  const { name, price } = req.body;
+  try {
+    await Product.create({
+      name: name,
+      price: price,
+      userId: req.userId,
+      role: req.role,
+    });
+    res.status(201).json({ msg: "product created successfully" });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+export const updateProducts = async (req = request, res = response) => {
+  try {
+    const product = await Product.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    if (!product) return res.status(404).json({ msg: "Data not found" });
+
+    const { name, price } = req.body;
+    if (req.role === "admin") {
+      await Product.update(
+        { name, price },
+        {
+          where: {
+            id: product.id,
+          },
+        }
+      );
+    } else {
+      if (req.userId !== product.userId)
+        return res.status(403).json({ msg: "you don't have access" });
+      await Product.update(
+        { name, price },
+        {
+          where: {
+            [Op.and]: [{ id: product.id }, { userId: req.userId }],
+          },
+        }
+      );
     }
-}
+    res.status(200).json({ msg: "Product updated successfuly" });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
 
-export const updateProducts = (req = request, res = response) => {
+export const deleteProducts = async (req = request, res = response) => {
+  try {
+    const product = await Product.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    if (!product) return res.status(404).json({ msg: "Data not found" });
 
-}
-
-export const deleteProducts = (req = request, res = response) => {
-
-}
+    if (req.role === "admin") {
+      await Product.destroy({
+        where: {
+          id: product.id,
+        },
+      });
+    } else {
+      if (req.userId !== product.userId)
+        return res.status(403).json({ msg: "you don't have access" });
+      await Product.destroy({
+        where: {
+          [Op.and]: [{ id: product.id }, { userId: req.userId }],
+        },
+      });
+    }
+    res.status(200).json({ msg: "Product deleted successfuly" });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
